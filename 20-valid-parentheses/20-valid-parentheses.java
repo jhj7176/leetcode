7df91ca167829f544314f422046a76724b1701ea
Hyeong-Jin Jeon <67823675+jhj7176@users.noreply.github.com>
2022-08-02 13:43:19 +0900

    Time: 4 ms (31.76%), Space: 42.1 MB (52.30%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,51 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
        
-       Boolean boo = true;
-		//String input = "{[[]()()[()]{}{}{()[]}]}";
-        String input = s;
-		StringBuilder sb1 = new StringBuilder();
-        
-		for (int i = 0; i < input.length(); i++) {
-			String chara = String.valueOf(input.charAt(i));
-			String lastChar = "";
-			if(sb1.length()!=0) lastChar = String.valueOf(sb1.charAt(sb1.length()-1));
-			sb1.append(chara);
-           if (chara.equals("}")) {
-
-				if (!lastChar.equals("{")) {
-					boo = false;
-					break;
-				} else {
-					sb1.delete(sb1.lastIndexOf("{}"), sb1.lastIndexOf("{}") + 2);
-
-				}
-
-			} else if (chara.equals(")")) {
-
-				if (!lastChar.equals("(")) {
-					boo = false;
-					break;
-				} else {
-					sb1.delete(sb1.lastIndexOf("()"), sb1.lastIndexOf("()") + 2);
-
-				}
-
-			} else if (chara.equals("]")) {
-
-				if (!lastChar.equals("[")) {
-					boo = false;
-					break;
-				} else {
-					sb1.delete(sb1.lastIndexOf("[]"), sb1.lastIndexOf("[]") + 2);
-
-				}
-
-			}
-
-		}
-        
-		if(!sb1.toString().equals("")) boo = false;
-        return boo;
+      Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if(a == '(' || a == '[' || a == '{') stack.push(a);
+            else if(stack.empty()) return false;
+            else if(a == ')' && stack.pop() != '(') return false;
+            else if(a == ']' && stack.pop() != '[') return false;
+            else if(a == '}' && stack.pop() != '{') return false;
+        }
+        return stack.empty();
     }
 }
